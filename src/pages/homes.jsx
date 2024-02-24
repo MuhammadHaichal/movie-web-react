b0b06b560c263f8a0cec21b8025667ed49b286b7
muhammadHaichal <localhostkali06@gmail.com>
2024-02-24 17:00:20 +0700

    menambahkan feature cardsearch di home

--- src/pages/homes.jsx
+++ src/pages/homes.jsx
@@ -2,11 +2,17 @@ import { useState } from "react";
 import axios from "axios";
 import SearchMovies from "../components/SearchMovies.jsx";
 import Container from "react-bootstrap/Container";
+import Card from 'react-bootstrap/Card';
 import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Stack from "react-bootstrap/Stack"
 
 const Homes = () => {
   const [keywordMovie, setKeywordMovie] = useState("");
-
+  const [movies, setMovies] = useState([])
+  
+  
   async function searchMovie(e) {
     e.preventDefault();
 
@@ -25,8 +31,12 @@ const Homes = () => {
     if (response.length == 0) {
       console.log("film tidak ditemukan");
     } else {
-      response.map((value) => setKeywordMovie(value));
+		setMovies(response)
     }
+  }
+  
+  async function detailHandller(movieID) {
+  	console.log({movieID: movieID})
   }
 
   return (
@@ -42,7 +52,37 @@ const Homes = () => {
       </div>
       {/* search movie end */}
 
-      <div></div>
+	  {/* card search movie start*/}
+      <div className="mt-4">
+    	<Container fluid="md">
+    		<Row className="justify-content-center">
+				{movies.map((movie) => (
+					<Col xs={12} sm={12} md="auto" key={movie.id}>
+					    <Card style={{ width: '18rem' }} className="m-3">
+					      <Card.Img variant="top" src={`${process.env.imageURL}${movie.poster_path}`} />
+					      <Card.Body>
+					        <Card.Title>{movie.title}</Card.Title>
+					        <Stack direction="horizontal" className="align-items-center justify-content-between">
+					        	<Card.Text>
+					        		Tahun Rilis <br />
+					        		{movie.release_date}
+					        	</Card.Text>
+					        	<Card.Text>
+					        		Bahasa <br />
+					        		{movie.original_language}
+					        	</Card.Text>
+					        </Stack>
+					        <Button onClick={(e) => detailHandller(movie.id) } variant="primary">
+					        	Detail Movie
+					        </Button>
+					      </Card.Body>
+					    </Card>
+					</Col>
+				))}
+    		</Row>
+    	</Container>
+      </div>
+      {/* card search movie end */}
     </>
   );
 };
