594d53627a02c4f55cec76302c5bad976bce6ddf
muhammadHaichal <localhostkali06@gmail.com>
2024-02-28 18:49:21 +0700

    memperbaiki kesalahan

--- src/components/SearchMovies.jsx
+++ src/components/SearchMovies.jsx
@@ -2,7 +2,6 @@ import Stack from "react-bootstrap/Stack";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
-const SearchMovies = (props) => {
   return (
     <>
       <div>
@@ -12,20 +11,21 @@ const SearchMovies = (props) => {
             gap={3}
             className="p-5 justify-content-center"
           >
-            <Form.Group controlId="exampleForm.ControlInput1">
+            <Form.Group className="w-full" controlId="exampleForm.ControlInput1">
               <Form.Control
+                className=" p-2"
                 type="text"
                 placeholder="Cari Movies ..."
                 required
                 onChange={(e) => props.onChange(e.target.value)}
               />
             </Form.Group>
-
+            
             <Form.Group>
-              <Button type="submit" variant="primary">
-                Cari
-              </Button>
+            	<Button className="p-2" type="submit" variant="primary">Search 🔎</Button>
             </Form.Group>
+           
+		    
           </Stack>
         </Form>
       </div>
--- src/pages/homes.jsx
+++ src/pages/homes.jsx
@@ -2,21 +2,20 @@ import { useState } from "react";
 import axios from "axios";
 import SearchMovies from "../components/SearchMovies.jsx";
 import Container from "react-bootstrap/Container";
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
-import Stack from "react-bootstrap/Stack"
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Stack from "react-bootstrap/Stack";
 
 const Homes = () => {
   const [keywordMovie, setKeywordMovie] = useState("");
-  const [movies, setMovies] = useState([])
-  
-  
+  const [movies, setMovies] = useState([]);
+
   async function searchMovie(e) {
     e.preventDefault();
 
-    const reqeust = await axios.get(
+    const request = await axios.get(
       `${process.env.baseURL}/search/movie?query=${keywordMovie}&include_adult=false&language=en-US&page=1`,
       {
         headers: {
@@ -26,17 +25,17 @@ const Homes = () => {
       },
     );
 
-    let response = reqeust.data.results;
+    let response = request.data.results;
 
     if (response.length == 0) {
       console.log("film tidak ditemukan");
     } else {
-		setMovies(response)
+      setMovies(response);
     }
   }
-  
+
   async function detailHandller(movieID) {
-  	console.log({movieID: movieID})
+    console.log({ movieID: movieID });
   }
 
   return (
@@ -45,42 +44,58 @@ const Homes = () => {
       <div>
         <div className="hero-bg">
           <div className=" pt-5">
-            <h1 className="text-center text-light"> Movie - Haikal </h1>
+            <h1 className="text-center text-light">
+              {" "}
+              Movie - <span className="text-warning"> Haikal </span>{" "}
+            </h1>
+            <p className="text-center text-light">
+              {" "}
+              Website Pencari Film atau Anime. By Haikal{" "}
+            </p>
           </div>
           <SearchMovies onSubmit={searchMovie} onChange={setKeywordMovie} />
         </div>
       </div>
       {/* search movie end */}
 
-	  {/* card search movie start*/}
-      <div className="mt-4">
-    	<Container fluid="md">
-    		<Row className="justify-content-center">
-				{movies.map((movie) => (
-					<Col xs={12} sm={12} md="auto" key={movie.id}>
-					    <Card style={{ width: '18rem' }} className="m-3">
-					      <Card.Img variant="top" src={`${process.env.imageURL}${movie.poster_path}`} />
-					      <Card.Body>
-					        <Card.Title>{movie.title}</Card.Title>
-					        <Stack direction="horizontal" className="align-items-center justify-content-between">
-					        	<Card.Text>
-					        		Tahun Rilis <br />
-					        		{movie.release_date}
-					        	</Card.Text>
-					        	<Card.Text>
-					        		Bahasa <br />
-					        		{movie.original_language}
-					        	</Card.Text>
-					        </Stack>
-					        <Button onClick={(e) => detailHandller(movie.id) } variant="primary">
-					        	Detail Movie
-					        </Button>
-					      </Card.Body>
-					    </Card>
-					</Col>
-				))}
-    		</Row>
-    	</Container>
+      {/* card search movie start*/}
+      <div className="mt-4 bg-light rounded ">
+        <Container fluid="md">
+          <Row className="justify-content-center">
+            {movies.map((movie) => (
+              <Col xs={12} sm={12} md="auto" key={movie.id}>
+                <Card style={{ width: "18rem" }} className="m-3">
+                  <Card.Img
+                    variant="top"
+                    src={`${process.env.imageURL}${movie.poster_path}`}
+                  />
+                  <Card.Body>
+                    <Card.Title>{movie.title}</Card.Title>
+                    <Stack
+                      direction="horizontal"
+                      className="align-items-center justify-content-between"
+                    >
+                      <Card.Text>
+                        Tahun Rilis <br />
+                        {movie.release_date}
+                      </Card.Text>
+                      <Card.Text>
+                        Bahasa <br />
+                        {movie.original_language}
+                      </Card.Text>
+                    </Stack>
+                    <Button
+                      onClick={(e) => detailHandller(movie.id)}
+                      variant="primary"
+                    >
+                      Detail Movie
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </div>
       {/* card search movie end */}
     </>
